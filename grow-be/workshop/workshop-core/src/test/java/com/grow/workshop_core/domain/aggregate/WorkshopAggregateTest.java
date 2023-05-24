63cc7135e31dee3177712c1296018271b3bfc061
Dell <tigranMkrtchyan.07.06@gmail.com>
2023-05-24 03:17:34 -0700

    add failed test

---
+++ grow-be/workshop/workshop-core/src/test/java/com/grow/workshop_core/domain/aggregate/WorkshopAggregateTest.java
@@ -0,0 +1,12 @@
+package com.grow.workshop_core.domain.aggregate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WorkshopAggregateTest {
+    @Test
+    void addWorkShopTest() {
+        assertThat(true).isFalse();
+    }
+}
